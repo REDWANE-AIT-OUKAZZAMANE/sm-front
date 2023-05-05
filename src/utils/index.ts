23887f664574620824a541e8d53d404eb34c687d
Oussama Aimarah <oussama.aimarah@x-hub.io>
2023-05-05 15:30:09 +0000

    Fix [SMW-86]: add case to remove animation when text contains hashtags only

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,17 @@
 import { socialMediaSources } from './constants';
 
+const shouldAnimate = (text: string): boolean => {
+  if (!text) return false;
+  return !text
+    .trim()
+    .split(' ')
+    .every((word) => word.startsWith('#'));
+};
+
 export const youtubeQueryParams = (media_url: string, id: string): string =>
   `${media_url}?autoplay=1&mute=1&loop=1&playlist=${id}`;
 
 export const getSocialMediaType = (caption: string, source: string): string =>
-  caption && source !== socialMediaSources.YOUTUBE ? 'animated' : 'static';
+  shouldAnimate(caption) && source !== socialMediaSources.YOUTUBE
+    ? 'animated'
+    : 'static';
