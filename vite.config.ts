34709d2336b7163e966e292ae3402e5ea143d845
El Mahdi Ben Moumen <htmidou@gmail.com>
2023-06-09 13:25:44 +0100

    add .env file and env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  build: {
-    rollupOptions: {
-      external: 'NonExistingPath',
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/ws': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react(), svgr()],
+    build: {
+      rollupOptions: {
+        external: 'NonExistingPath',
       },
     },
-  },
-});
+    server: {
+      port: 3000,
+      proxy: {
+        '/ws': {
+          target: process.env.VITE_APP_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+        '/api': {
+          target: process.env.VITE_APP_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+};
