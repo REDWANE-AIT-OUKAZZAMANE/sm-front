26ef48e8f5c937269799a30b43b5c28750a5878b
Redwane Ait Oukazzamane <redwane.aitoukazzamane@x-hub.io>
2023-06-12 15:10:39 +0000

    Fix[SMW-194]: wall settings title regex

--- src/utils/RegexPatterns.ts
+++ src/utils/RegexPatterns.ts
@@ -2,7 +2,7 @@ const RegexPatterns = Object.freeze({
   email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
   password:
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-  wallTitle: /^(?=.*[a-z]).{5,}$/,
+  wallTitle: /^[\s\S]{5,}$/,
   announcementTitle: /^[a-zA-Z0-9' -]{5,}$/,
 });
 
