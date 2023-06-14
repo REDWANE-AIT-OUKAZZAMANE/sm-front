b7eaa66808e327cedd716125cd316a43884a1503
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2023-06-14 12:13:51 +0100

    add async-states-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { autoConfigureDevtools } from 'async-states-devtools';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import 'async-states-devtools/dist/style.css';
 import App from './App';
 import './App.css';
+
+autoConfigureDevtools({ open: false });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
